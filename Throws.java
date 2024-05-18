ab3a2449cd9a3090c44df78c391577933cb90bb1
Dabikar Sreenija <168444765+dabikar-sreenija@users.noreply.github.com>
2024-05-18 20:31:45 +0530

    Add files via upload

---
+++ Throws.java
@@ -0,0 +1,25 @@
+ package exception;
+import java.io.IOException;
+
+public class Throws {
+	void m()throws IOException{
+throw new IOException("device error");
+}
+	void n()throws IOException{
+		m();
+	}
+	void p() {
+		try {
+			n();
+		}
+		catch(Exception e) {
+			System.out.println("exception handled");
+		}
+	}
+	public static void main(String[] args) {
+		Throws obj=new Throws();
+		obj.p();
+		System.out.println("normal flow..");
+	}
+}
+ 
